6aab26993426681fe2ad23ff8ec5811376aaefaf
jenkomq <144250143+jenkomq@users.noreply.github.com>
2024-04-23 15:47:21 +0300

    Delete src directory

--- src/main/java/Entity/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-import com.example.demo.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
