a64a0b67788b0df234a0eba8cbb9321b5cc561ce
jenkomq <144250143+jenkomq@users.noreply.github.com>
2024-04-24 18:12:58 +0300

    Add files via upload

---
+++ demo7/src/main/java/com/example/model/User.java
@@ -0,0 +1,19 @@
+package main.java.com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Геттеры и сеттеры
+}
