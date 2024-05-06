3ca0232086de31d82622d79a95318b986dcb3b44
jenkomq <144250143+jenkomq@users.noreply.github.com>
2024-05-06 14:24:02 +0300

    Update CurrencyExchangeServiceSampleApplication.java

--- Project/CurrencyExchangeServiceSampleApplication.java
+++ Project/CurrencyExchangeServiceSampleApplication.java
@@ -2,9 +2,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
-// It is equivalent to using @Configuration,
-// @EnableAutoConfiguration and @ComponentScan with their
-// default attributes:
 public class CurrencyExchangeServiceSampleApplication {
  
     public static void main(String[] args)
