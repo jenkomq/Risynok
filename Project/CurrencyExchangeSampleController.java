5eac4c69b015f9bd672b10cc89cf26bcd213f2eb
jenkomq <144250143+jenkomq@users.noreply.github.com>
2024-05-06 14:23:35 +0300

    Create CurrencyExchangeSampleController.java

---
+++ Project/CurrencyExchangeSampleController.java
@@ -0,0 +1,2 @@
+@GetMapping("/currency-exchange-sample/fromCurrency/{fromCurrency}/toCurrency/{toCurrency}") 
+// fromCurrency может быть долларом США, евро, австралийским долларом, индийской рупией, а toCurrency может быть противоположностью любой fromCurrency
